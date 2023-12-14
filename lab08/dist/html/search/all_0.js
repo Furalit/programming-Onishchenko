36dc30267386c0073e7541ac65071e6f74231d49
Furalit <Furalit@gmail.com>
2023-12-14 13:40:15 +0200

    Prime number3

---
+++ lab08/dist/html/search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['getnum_0',['getNum',['../main_8c.html#a5d06a1578648c9b23746188891b01aae',1,'main.c']]]
+];
---
+++ lab08/dist/html/search/all_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['isprime_1',['isPrime',['../main_8c.html#a03175512d4c4876cf35791cdfa0301ff',1,'main.c']]]
+];
